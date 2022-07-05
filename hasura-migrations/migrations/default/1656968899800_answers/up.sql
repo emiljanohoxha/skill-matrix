69983ac1873c1a809ce0104661b238d434c410f9
albi-mema <amema@unitsystem.net>
2022-07-05 14:00:12 +0000

    added json column to answers table

--- hasura-migrations/migrations/default/1656968899800_answers/up.sql
+++ hasura-migrations/migrations/default/1656968899800_answers/up.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS "public"."answers"(
     "updated_at" TIMESTAMP,
     "SCORE" INT,
     "NOTES" varchar,
+    "data" JSON NOT NULL DEFAULT '{}',
     CONSTRAINT "answers_pkey" PRIMARY KEY ("answer_id"),
     FOREIGN KEY(board_id) REFERENCES boards(board_id),
     FOREIGN KEY(user_id) REFERENCES users(user_id) -- FOREIGN KEY(question_id) REFERENCES questions(question_id)
