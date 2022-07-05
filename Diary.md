da436c1306b2eaf32a5ae6d1c33ac103ecbc5548
emiljanohoxha <hoxhaemiljano6@gmail.com>
2022-07-05 08:12:44 +0000

    planted some seeds and created the diary

---
+++ Diary.md
@@ -0,0 +1,6 @@
+## Day 0
+
+--Brainstorming with the team to understand better the project and how we are going to proceed in the upcoming days.
+--Created a database schema design using diagraming tool.
+--Applied sql migrations based on schema provided by Marco.
+--Planted some seeds.
--- hasura-migrations/seeds/default/dummy.sql
+++ hasura-migrations/seeds/default/dummy.sql
@@ -1,1 +1,31 @@
-SELECT NOW();
+-- INSERT INTO "public"."users" ( "name")
+-- VALUES
+--   ( 'user2')
+-- , ('user3')
+-- , ( 'user4')
+
+-- ON CONFLICT ON CONSTRAINT "users_pkey"
+-- DO UPDATE SET "name" = EXCLUDED."name";
+
+
+-- INSERT INTO "public"."boards" ( "name")
+-- VALUES
+--   ( 'board1')
+-- , ( 'board2')
+-- , ( 'board3')
+-- , ( 'board4')
+-- , ( 'board5')
+-- -- handle conflicts:
+-- ON CONFLICT ON CONSTRAINT "boards_pkey"
+-- DO UPDATE SET "name" = EXCLUDED."name";
+
+-- INSERT INTO "public"."surveys" ( "board_id")
+-- VALUES
+--   ( '1')
+-- , ( '2')
+-- , ( '3')
+-- , ( '4')
+-- , ( '5')
+-- -- handle conflicts:
+-- ON CONFLICT ON CONSTRAINT "surveys_pkey"
+-- DO UPDATE SET "board_id" = EXCLUDED."board_id";
