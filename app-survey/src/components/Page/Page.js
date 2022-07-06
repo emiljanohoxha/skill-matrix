f85f7a024231e211c20b17a56936d448bf3cf338
albi-mema <amema@unitsystem.net>
2022-07-06 20:14:29 +0000

    changed the starter login page

--- app-survey/src/components/Page/Page.js
+++ app-survey/src/components/Page/Page.js
@@ -1,6 +1,8 @@
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import StartIcon from "@mui/icons-material/Start";
 
 export const PageTitle = ({ actions, children }) => (
   <Box
@@ -18,35 +20,79 @@ export const PageTitle = ({ actions, children }) => (
         }}
       >
         {typeof children === "string" ? (
-          <Typography variant="h5">{children}</Typography>
+          <Typography
+            variant="h3"
+            sx={{
+              display: "flex",
+
+              justifyContent: "center"
+            }}
+          >
+            {children}
+          </Typography>
         ) : (
           children
         )}
       </Box>
     )}
-
     {actions && <Box sx={{ ml: 4 }}>{actions}</Box>}
   </Box>
 );
 
-export const Page = ({ title, actions, withPadding, children, ...props }) => (
+export const Page = ({
+  title,
+  actions,
+  withPadding,
+  actions2,
+  children,
+  ...props
+}) => (
   <Paper
     {...props}
     sx={{
-      width: "95vw",
+      width: "75vw",
       minHeight: "100%"
     }}
   >
     {(title || actions) && <PageTitle actions={actions}>{title}</PageTitle>}
     <Box
       sx={{
-        pl: withPadding ? 2 : 0,
-        pr: withPadding ? 2 : 0,
-        pt: withPadding ? 2 : 0,
-        pb: withPadding ? 2 : 0
+        display: "flex",
+        justifyContent: "center"
       }}
     >
-      {children}
+      <Box
+        sx={{
+          p: withPadding ? 20 : 0,
+
+          backgroundColor: "#e6e8e6",
+
+          borderBottom: "2px solid black"
+        }}
+      >
+        We're a startup company looking to learn more about how women buy
+        wedding dresses. Reflect back to the time if and when you were looking
+        for a wedding dress, and try to approach this survey through those eyes.
+      </Box>
+    </Box>
+
+    {/* {actions && <Box sx={{ ml: 4 }}>{actions}</Box>} */}
+
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        mt: 5
+      }}
+    >
+      <Button
+        color="primary"
+        variant="outlined"
+        endIcon={<StartIcon />}
+        actions={actions}
+      >
+        Start
+      </Button>
     </Box>
   </Paper>
 );
