e58f626a63a67de4e842910eec200641b72a4069
albi-mema <amema@unitsystem.net>
2022-07-07 15:24:59 +0000

    changed questions table structure and created a query

--- app-survey/src/features/app/App.js
+++ app-survey/src/features/app/App.js
@@ -1,52 +1,58 @@
 import { useQuery, gql } from "../../services/hasura-client";
 import Page from "../../components/Page";
 import { Logout } from "./Logout";
-
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Survey } from "../../components/Page/Survey";
 
 const PING_ACTION_QUERY = gql`
   query MyQuery {
     questions {
-      data,
+      data
       type
     }
   }
 `;
 
 const query = gql`
-query MyQuery1 {
-    answers(where: {user_id: {_eq: 1}}) {
-      NOTES
-      SCORE
-      board_id
-      created_at
-      question_id
-      updated_at
-      user_id
+  query MyQuery1 {
+    questions {
+      data
+      type
+      board {
+        answers(where: { question_id: { _eq: 1 } }) {
+          NOTES
+          SCORE
+          board_id
+          created_at
+          question_id
+          updated_at
+          user_id
+        }
+      }
     }
   }
 `;
 
-
 export const App = () => {
-  const { isSuccess,  data } = useQuery("MyQuery", PING_ACTION_QUERY);
-  // const test1 = useQuery("MyQuery1", query);
-  console.log(data);
-  // console.log("my data",test1.data);
-
+  const { isSuccess, data } = useQuery("MyQuery", PING_ACTION_QUERY);
+  const test1 = useQuery("MyQuery1", query);
+  // console.log(data);
+  console.log("my data", test1.data);
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Page withPadding title={"Survey App"} actions={<Logout />}> 
-        {isSuccess
-          ? `Computer says: ${data.questions[0].data.text}`
-          : "loading time..."}
-      </Page>}/>
-      <Route path="/survey" element={<Survey item={data} />} />
-      
-    </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Page withPadding title={"Survey App"} actions={<Logout />}>
+              {isSuccess
+                ? `Computer says: ${data.questions[0].data.text}`
+                : "loading time..."}
+            </Page>
+          }
+        />
+        <Route path="/survey" element={<Survey item={data} />} />
+      </Routes>
     </>
-
   );
 };
--- hasura-migrations/migrations/default/1656966697196_questions/up.sql
+++ hasura-migrations/migrations/default/1656966697196_questions/up.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE "public"."questions" (
   "question_id" INT GENERATED ALWAYS AS IDENTITY,
   "board_id" INT NOT NULL,
@@ -6,6 +5,6 @@ CREATE TABLE "public"."questions" (
   "data" JSON NOT NULL,
   "is_deleted" BOOLEAN NOT NULL DEFAULT false,
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT clock_timestamp(),
-  CONSTRAINT "questions_pkey" PRIMARY KEY ("question_id", "created_at"),
+  CONSTRAINT "questions_pkey" PRIMARY KEY ("question_id"),
   CONSTRAINT "questions_board_id_fkey" FOREIGN KEY("board_id") REFERENCES "boards"("board_id")
 ) WITH (fillfactor = 100);
